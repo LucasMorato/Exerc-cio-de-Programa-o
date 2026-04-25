0c6f29610d5f11b81b0513cb63c643079df5e359
Lucas Cantamessa Morato <lucascm12@al.insper.edu.br>
2026-04-25 14:46:45 -0300

    Funcao Ex 6 - V1

--- funcoes.py
+++ funcoes.py
@@ -50,6 +50,28 @@ def calcula_pontos_soma(lista_faces):
     for elemento in lista_faces:
         pontos += elemento
     return pontos
+
+def calcula_pontos_sequencia_baixa(lista_faces):
+    pontos = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
+    for elemento in lista_faces:
+        if elemento==1:
+            pontos[1] += 1
+        elif elemento==2:
+            pontos[2] += 1
+        elif elemento==3:
+            pontos[3] += 1
+        elif elemento==4:
+            pontos[4] += 1
+        elif elemento==5:
+            pontos[5] += 1
+        elif elemento==6:
+            pontos[6] += 1
+    if pontos[1] >= 1 and pontos[2] >= 1 and pontos[3] >= 1 and pontos[4] >= 1:
+        return 15
+    elif pontos[2] >= 1 and pontos[3] >= 1 and pontos[4] >= 1 and pontos[5] >= 1:
+        return 15
+    elif pontos[3] >= 1 and pontos[4] >= 1 and pontos[5] >= 1 and pontos[6] >= 1:
+        return 15
         
     
         
